2729833b5a7a331d56d84b113648758b692b21aa
Venilson Costa Neves <venilsoncosta21@gmail.com>
2025-05-05 07:52:34 -0300

    feat: Adiciona métodos getter e setter para o atributo nome na classe Teste

--- src/main/java/com/venilson/craque_das_letras/models/entity/Teste.java
+++ src/main/java/com/venilson/craque_das_letras/models/entity/Teste.java
@@ -2,4 +2,12 @@ package com.venilson.craque_das_letras.models.entity;
 
 public class Teste {
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
